f71786e723b0995e3295763fbf6b22aad36b609e
Wavelink <wavelink@email.cz>
2023-01-07 22:02:16 +0100

    init

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,10 @@
 import type { LoadEvent } from "@sveltejs/kit";
 
 export function load({ url }: LoadEvent) {
-  if(url.hostname.includes("youtube.com")) {
+  console.log(url);
+  
+
+  if(url.host.includes("youtube.com")) {
     const id = url.searchParams.get("v");
     return {
       status: 307,
